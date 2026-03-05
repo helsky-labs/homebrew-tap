90076e8cc5b3a263b8706d7795db5102afc91d37
hel rabelo <helrabelo@gmail.com>
2026-03-05 14:31:46 -0300

    chore: bump tokencap to 1.3.1

--- Casks/tokencap.rb
+++ Casks/tokencap.rb
@@ -1,6 +1,6 @@
 cask "tokencap" do
-  version "1.4.1"
-  sha256 "5af314ffabf332053454829e7d7e8f42207da797040ae91cedc7a3d47c584ee1"
+  version "1.3.1"
+  sha256 "7495d85a420a9ce3c630d3c0fd615d1ccc3f70c3b6d3a75f1552ba91617ad72c"
 
   url "https://github.com/helsky-labs/tokencap/releases/download/v#{version}/TokenCap-v#{version}.dmg"
   name "TokenCap"
